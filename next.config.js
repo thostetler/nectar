e03f6f61f82edd976e69fc69903f1377d360c4b4
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2021-11-04 11:42:10 -0400

    add sentryjs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
+const { withSentryConfig } = require('@sentry/nextjs');
 
 /**
  * @type {import('next').NextConfig}
@@ -26,4 +27,16 @@ const config = {
   },
 };
 
-module.exports = withBundleAnalyzer(config);
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+module.exports = withSentryConfig(withBundleAnalyzer(config), sentryWebpackPluginOptions);
