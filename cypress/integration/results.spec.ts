c1294fd027f23c64494a4dd3dd40e13fd54dd4a7
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-03-23 05:30:28 -0400

    wip

--- cypress/integration/results.spec.ts
+++ cypress/integration/results.spec.ts
@@ -1,32 +1,60 @@
-import { beforeEach, cy, describe, it } from 'local-cypress';
+import { IADSApiSearchParams } from '@api/lib/search/types';
+import { cy, describe, expect, it } from 'local-cypress';
+
+const search = (params: Partial<IADSApiSearchParams>) => {
+  cy.visit('/search', {
+    qs: params,
+  });
+};
+
+const updateAndSubmitForm = (value: string) => {
+  cy.get('@input', { timeout: 10000 }).should('be.enabled').type(value);
+  cy.get('form').submit();
+  cy.get('@input', { timeout: 10000 }).should('be.enabled');
+};
+
+const assertQueryParam = (checks: Record<string, string>) => {
+  cy.location().then((location) => {
+    const params = new URLSearchParams(location.search);
+    Object.keys(checks).forEach((key) => expect(checks[key]).eq(params.get(key), `checking query param (${key})`));
+  });
+};
 
 describe('Results Page URL sync', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:8000/search', {
-      qs: {
-        q: 'star',
-        sort: 'date desc',
-        p: 1,
-      },
-    });
-  });
   it('searchbar shows correct search term', () => {
-    cy.get('form').find('[type="text"]').first().should('have.value', 'star');
+    search({ q: 'a' });
+    cy.get('form').find('[type="text"]').first().should('have.value', 'a');
   });
 
-  it.only('submission updates url correctly', () => {
-    cy.wait(500);
-    cy.get('form').find('[type="text"]').first().as('input');
-    cy.get('@input').type(' a');
-    cy.get('form').submit();
-    cy.wait(500);
-    cy.get('@input').type(' b');
-    cy.get('form').submit();
-    cy.wait(500);
-    cy.get('@input').type(' c');
-    cy.get('form').submit();
-    cy.wait(500);
-    cy.get('@input').type(' d');
-    cy.get('form').submit();
+  it('submission updates url correctly', () => {
+    search({ q: 'a' });
+    cy.get('form').find('[type="text"]', { timeout: 10000 }).first().as('input').should('be.enabled');
+    cy.get('@input').should('have.value', 'a');
+
+    updateAndSubmitForm(' b');
+    assertQueryParam({ q: 'a b' });
+    cy.get('@input').should('have.value', 'a b');
+
+    updateAndSubmitForm(' c');
+    assertQueryParam({ q: 'a b c' });
+    cy.get('@input').should('have.value', 'a b c');
+  });
+
+  it.only('history updates url correctly', () => {
+    search({ q: 'a' });
+    cy.get('form').find('[type="text"]', { timeout: 10000 }).first().as('input').should('be.enabled');
+    updateAndSubmitForm(' b');
+    updateAndSubmitForm(' c');
+
+    assertQueryParam({ q: 'a b c' });
+    cy.get('@input').should('have.value', 'a b c');
+
+    cy.go('back');
+    assertQueryParam({ q: 'a b' });
+    cy.get('@input').should('have.value', 'a b');
+
+    cy.go('back');
+    assertQueryParam({ q: 'a' });
+    cy.get('@input').should('have.value', 'a');
   });
 });
