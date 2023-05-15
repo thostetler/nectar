0a95da0254f638da910e9351b86634243b60f209
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2023-05-15 14:12:23 -0400

    wip

--- src/trpc-utils.ts
+++ src/trpc-utils.ts
@@ -1,29 +1,9 @@
 import { httpBatchLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
 import type { AppRouter } from '@server/_app';
-import { QueryCache, QueryClient, QueryClientConfig } from 'react-query';
+import { QueryCache, QueryClientConfig } from 'react-query';
 import axios from 'axios';
 import superjson from 'superjson';
-
-function getBaseUrl() {
-  if (typeof window !== 'undefined') {
-    // browser should use relative path
-    return '';
-  }
-
-  if (process.env.VERCEL_URL) {
-    // reference for vercel.com
-    return `https://${process.env.VERCEL_URL}`;
-  }
-
-  if (process.env.RENDER_INTERNAL_HOSTNAME) {
-    // reference for render.com
-    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
-  }
-
-  // assume localhost
-  return `http://localhost:${process.env.PORT ?? 3000}`;
-}
 
 const queryClientConfig: QueryClientConfig = {
   defaultOptions: {
@@ -52,14 +32,14 @@ export const trpc = createTRPCNext<AppRouter>({
     return {
       transformer: superjson,
       queryClientConfig,
-      queryClient: new QueryClient(),
+      // queryClient: new QueryClient(queryClientConfig),
       links: [
         httpBatchLink({
           /**
            * If you want to use SSR, you need to use the server's full URL
            * @link https://trpc.io/docs/ssr
            **/
-          url: `${getBaseUrl()}/api/trpc`,
+          url: ``,
 
           // You can pass any HTTP headers you wish here
           async headers() {
