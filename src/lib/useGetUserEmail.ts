833f3582f835e845a42aada4658570d62dcd9d2c
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2024-05-10 15:47:44 -0400

    Fix possible undefined value in username hook

--- src/lib/useGetUserEmail.ts
+++ src/lib/useGetUserEmail.ts
@@ -1,6 +1,5 @@
 import { useStore } from '@/store';
 
 export const useGetUserEmail = () => {
-  const username = useStore((state) => state.user.username);
-  return username !== 'anonymous@ads' ? username : null;
+  return useStore((state) => state.user?.username ?? 'anonymous@ads');
 };
